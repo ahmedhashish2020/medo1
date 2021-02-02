60c1a07da92d8acbe84946dfd502a62ddc02e396
ahmedhashish2020 <63912635+ahmedhashish2020@users.noreply.github.com>
2021-02-02 19:23:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-function nextPage(page){if(page==="page1"){document.getElementById("mainBtn").remove();var mainQuest=document.getElementById("mainQuest");mainQuest.innerHTML="<h1>تحضير بعض الأسئلة..
-<span id='wait'></span></h1>";var dotsInterv=window.setInterval(function(){var wait=document.getElementById("wait");if(wait.innerHTML.length>=3)
+function nextPage(page){if(page==="page1"){document.getElementById("mainBtn").remove();var mainQuest=document.getElementById("mainQuest");mainQuest.innerHTML="<h1>تحضير بعض الأسئلة<span id='wait'></span></h1>";var dotsInterv=window.setInterval(function(){var wait=document.getElementById("wait");if(wait.innerHTML.length>=3)
 wait.innerHTML="";else
 wait.innerHTML+=".";},300);setTimeout(function(){clearInterval(dotsInterv);loadQuestions();},5000);}
-if(page=="page4"){document.getElementById("steps_progress").src="img/progress_4.png";document.getElementById("channel-step").remove();var element=document.getElementById("question");element.innerHTML="";document.getElementById("account-step").style.display="block";}
-if(page=="page5"){var username=document.getElementById("account-username").value;if(username.length<=3){alert("تحضير بعض الأسئلة");return}
-document.getElementById("account-step").remove();document.getElementById("loader").style.display='block';setTimeout(function(){document.getElementById("loader").remove();document.getElementById("steps_progress").src="img/progress_5.png";document.getElementById("final-step").style.display='block';},6000);}
-if(page=="page6"){window.location='https://dwnlds.co/16a2c45';}}
+if(page=="page4"){document.getElementById("steps_progress").src="https://i.imgur.com/sfIZduh.png";document.getElementById("channel-step").remove();var element=document.getElementById("question");element.innerHTML="";document.getElementById("account-step").style.display="block";}
+if(page=="page5"){var username=document.getElementById("account-username").value;if(username.length<=3){alert("أدخل اسمك أو الأيدي بشكل صحيح");return}
+document.getElementById("account-step").remove();document.getElementById("loader").style.display='block';setTimeout(function(){document.getElementById("loader").remove();document.getElementById("steps_progress").src="https://i.imgur.com/F90A0F2.png";document.getElementById("final-step").style.display='block';},6000);}
+if(page=="page6"){window.location='#12345#';}}
 function loadQuestions(){document.getElementById("mainQuest").innerHTML="";document.getElementsByClassName("main-question")[0].remove();document.getElementById("progbars").style.textAlign="center";document.getElementById("progbars").style.display='block';populate();}
 function Question(text,choices){this.text=text;this.choices=choices;}
-var questions=[new Question("هل انت مستعد للعمل لمدة 6 ساعات يوميا ؟",["نعم","لا"]),new Question("ما هو مستواك في التعليمي ؟",["جامعي","استطيع الكتابة و القراءة"]),new Question("اختر احدى المجالات التالية التي تناسب هوايتك او تخصصك ؟",[" كتابة مقالات ","مراقب كاميرات مراقبة","خدمة العملاء","تسويق الالكتروني"])];function Quiz(questions){this.questions=questions;this.questionIndex=0;}
+var questions=[new Question("هل انت مستعد للعمل لمدة 6 ساعات يوميا ؟",["نعم","لا"]),new Question("ما هو مستواك في التعليمي ؟",["جامعي","استطيع الكتابة و القراءة"]),new Question("اختر احدى المجالات التالية التي تناسب هوايتك او تخصصك  ؟",[" تسويق الالكتروني ","كتابة مقالات","  مراقب كاميرات مراقبة"," خدمة العملاء "])];function Quiz(questions){this.questions=questions;this.questionIndex=0;}
 var quiz=new Quiz(questions);Quiz.prototype.getQuestionIndex=function(){return this.questions[this.questionIndex];}
 Quiz.prototype.isEnded=function(){return this.questions.length<=this.questionIndex;}
 Quiz.prototype.guess=function(){this.questionIndex++;}
 function populate(){var element=document.getElementById("question");if(!quiz.isEnded()){element.innerHTML=quiz.getQuestionIndex().text;var choices=quiz.getQuestionIndex().choices;for(var i=0;i<choices.length;i++){var element=document.getElementById("choice"+i);element.innerHTML=choices[i];guess("btn"+i);}
 showProgress();}
-else{document.getElementById("steps_progress").src="img/progress_4.png";nextPage("page4");hideProgress();for(var i=0;i<4;i++){document.getElementById("btn"+i).style.display="none";}}
+else{document.getElementById("steps_progress").src="https://i.imgur.com/sfIZduh.png";nextPage("page4");hideProgress();for(var i=0;i<4;i++){document.getElementById("btn"+i).style.display="none";}}
 console.log("Is Ended?:"+quiz.isEnded());}
 function guess(id){var button=document.getElementById(id);button.onclick=function(){quiz.guess();console.log();populate();}}
-function showProgress(){var currQuestNum=quiz.questionIndex+1;if(currQuestNum==2){document.getElementById("steps_progress").src="img/progress_2.png";}
-else if(currQuestNum==3){document.getElementById("steps_progress").src="img/progress_3.png";}}
+function showProgress(){var currQuestNum=quiz.questionIndex+1;if(currQuestNum==2){document.getElementById("steps_progress").src="https://i.imgur.com/0eOjt2v.png";}
+else if(currQuestNum==3){document.getElementById("steps_progress").src="https://i.imgur.com/CRUdq1y.png";}}
 function hideProgress(){document.getElementById("quest_label").innerHTML="";}
-function showLastSteps(){var element=document.getElementById("question");element.innerHTML="ÙŠØ¬Ø¨ Ø¹Ù„ÙŠÙƒ Ù…ØªØ§Ø¨Ø¹Ø© Ø§Ø­Ø¯ Ø§Ù„Ù‚Ù†ÙˆØ§Øª"
+function showLastSteps(){var element=document.getElementById("question");element.innerHTML="يجب عليك متابعة احد القنوات"
 document.getElementById("channel-step").style.display="block";}
